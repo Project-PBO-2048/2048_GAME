529ea6fe8d8b7bf493f7de3c56c6074ca21d3323
adikencana93 <116042445+adikencana93@users.noreply.github.com>
2023-12-18 15:24:36 +0700

    Add files via upload

---
+++ 2048_GAME/src/Api/Score.java
@@ -0,0 +1,73 @@
+package Api;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class Score {
+    public static int userScore;
+    public static int userPrevScore;
+
+    public static ArrayList<String> usernames = new ArrayList<>();
+    public static ArrayList<Integer> highscores = new ArrayList<>();
+
+    public static int getScore() {
+        return userScore;
+    }
+    public static int getPrevScore() {
+        return userPrevScore;
+    }
+
+    public static void updateScore(int update) {
+        userPrevScore=userScore;
+
+        userScore+=update;
+        System.out.println("user score : " + userScore);
+    }
+    public static void setUserScore(int score){
+        userScore=score;
+    }
+
+    public static void updateScore() throws SQLException {
+        System.out.println("user id : " + JDBC.user_id);
+        System.out.println("user score : " + userScore);
+        String getHighestScoreQuery = "SELECT MAX(score) AS highest_score FROM score WHERE player_id = '" + JDBC.user_id + "'";
+
+
+        int highestScore = 0; // Skor tertinggi default
+        try (Statement stmt = JDBC.client.createStatement()) {
+            ResultSet rs = stmt.executeQuery(getHighestScoreQuery);
+            if (rs.next()) {
+                highestScore = rs.getInt("highest_score");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        if (userScore > highestScore) {
+            String updateScoreQuery = "UPDATE score SET score = " + userScore + " WHERE player_id = '" + JDBC.user_id + "'";
+            System.out.println("hhhhhmmm " + userScore);
+
+            try (Statement stmt = JDBC.client.createStatement()) {
+                stmt.executeUpdate(updateScoreQuery);
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    public static void getUserScore() throws SQLException {
+        String query = "SELECT u.username, s.score FROM users u INNER JOIN score s ON u.id = s.player_id ORDER BY s.score DESC";
+        usernames.clear();
+        highscores.clear();
+        try (Statement stmt = JDBC.client.createStatement()) {
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()){
+                usernames.add(rs.getString("username"));
+                highscores.add(rs.getInt("score"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ 2048_GAME/src/Main.java
@@ -0,0 +1,30 @@
+import Api.JDBC;
+import Game.Board;
+import Game.MainMenu;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        new JDBC();
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setTitle("2048");
+
+        MainMenu mainMenu = new MainMenu();
+        window.add(mainMenu);
+        window.pack();
+
+        ImageIcon icon = new ImageIcon("D:\\unram\\TUGAS\\SEMESTER 5\\PBO\\PRAKTIKUM\\2048\\2048\\GAME2048\\src\\Assets\\icon.png");
+        Image image = icon.getImage();
+
+        window.setIconImage(image);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+
+
+    }
+}
