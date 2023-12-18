8ecc6a580bbe26acff9af2fc4f47603d1f710cd2
AdhityP <121078703+AdhityP@users.noreply.github.com>
2023-12-18 15:53:21 +0800

    Add files via upload

---
+++ 2048/GAME2048/out/production/GAME2048/game_2048.sql
@@ -0,0 +1,171 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 16, 2023 at 06:33 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `game_2048`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `score`
+--
+
+CREATE TABLE `score` (
+  `id` int(4) NOT NULL,
+  `player_id` int(4) NOT NULL,
+  `score` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `score`
+--
+
+INSERT INTO `score` (`id`, `player_id`, `score`) VALUES
+(161, 162, 0),
+(162, 163, 0),
+(163, 164, 0),
+(164, 165, 0),
+(165, 166, 0),
+(166, 167, 0),
+(167, 168, 0),
+(168, 169, 0),
+(169, 170, 0),
+(170, 171, 0),
+(171, 172, 0),
+(172, 173, 0),
+(173, 174, 0),
+(174, 175, 0),
+(175, 176, 0),
+(176, 177, 0),
+(177, 178, 0),
+(178, 179, 0),
+(179, 180, 28),
+(180, 181, 0),
+(181, 182, 0),
+(182, 183, 0),
+(183, 184, 0),
+(184, 185, 0),
+(185, 186, 0),
+(186, 187, 420),
+(187, 188, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(4) NOT NULL,
+  `created_at` date NOT NULL DEFAULT current_timestamp(),
+  `username` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `created_at`, `username`) VALUES
+(162, '2023-12-16', 'Dhira'),
+(163, '2023-12-16', 'Rama'),
+(164, '2023-12-16', 'hgc'),
+(165, '2023-12-16', 'asdas'),
+(166, '2023-12-16', 'asdas'),
+(167, '2023-12-16', 'asdas'),
+(168, '2023-12-16', 'asdas'),
+(169, '2023-12-16', 'asdas'),
+(170, '2023-12-16', 'asdas'),
+(171, '2023-12-16', 'asdas'),
+(172, '2023-12-16', '45454'),
+(173, '2023-12-16', 'Rama'),
+(174, '2023-12-16', 'sda'),
+(175, '2023-12-17', 'asd'),
+(176, '2023-12-17', 'asdas'),
+(177, '2023-12-17', 'sa'),
+(178, '2023-12-17', 'asd'),
+(179, '2023-12-17', 'Rama'),
+(180, '2023-12-17', 'RAma'),
+(181, '2023-12-17', 'RAma'),
+(182, '2023-12-17', 'RAma'),
+(183, '2023-12-17', 'Hmm'),
+(184, '2023-12-17', 'Dhira'),
+(185, '2023-12-17', 'a'),
+(186, '2023-12-17', 'Dhira'),
+(187, '2023-12-17', 'wertyui'),
+(188, '2023-12-17', '123');
+
+--
+-- Triggers `users`
+--
+DELIMITER $$
+CREATE TRIGGER `add_default_scores_trigger` AFTER INSERT ON `users` FOR EACH ROW BEGIN
+    INSERT INTO score (player_id, score)
+    VALUES (NEW.id, 0);
+END
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `score`
+--
+ALTER TABLE `score`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `player_id` (`player_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `score`
+--
+ALTER TABLE `score`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=188;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=189;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `score`
+--
+ALTER TABLE `score`
+  ADD CONSTRAINT `score_ibfk_1` FOREIGN KEY (`player_id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ 2048/GAME2048/src/Game/Leaderboard.java
@@ -0,0 +1,84 @@
+package Game;
+
+import Api.Score;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Leaderboard extends JPanel {
+
+    public final int screenWidth = 485; // 768 pixels
+    private final int screenHeight = 658;
+    private String[] username;
+    private Integer[] highscore;
+    public BufferedImage background;
+    public Leaderboard(){
+
+//        background = setup("/bg/leaderBoard.png");
+        this.setBackground(Color.decode("#fbf8ef"));
+        try{
+            Score.getUserScore();
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        setLayout(new FlowLayout(FlowLayout.CENTER, 0, 8));
+
+        JPanel gap = new JPanel();
+        gap.setPreferredSize(new Dimension(350, 100));
+        gap.setOpaque(false);
+        add(gap);
+
+        for (int i = 0; i < 10; i++) {
+            if (i<Score.usernames.size()){
+                JPanel userPanel = new JPanel();
+//                userPanel.setOpaque(false);
+                userPanel.setPreferredSize(new Dimension(350, 45));
+                userPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 20, 5));
+//                userPanel.setOpaque(false);
+                userPanel.setBackground(Color.decode("#8d6731"));
+                userPanel.setBorder(BorderFactory.createMatteBorder(3, 3, 3, 3, Color.decode("#8d6731")));
+
+                JLabel nameLabel = new JLabel(Score.usernames.get(i));
+                JLabel number = new JLabel(i+1+"");
+                JLabel score = new JLabel(Score.highscores.get(i)+"");
+
+                number.setPreferredSize(new Dimension(30, 30));
+//                number.setOpaque(true);
+                number.setHorizontalAlignment(JLabel.CENTER);
+//                number.setBackground(Color.decode("#e1a23a"));
+                number.setFont(new Font("Moonrising", Font.BOLD, 16));
+                number.setForeground(Color.WHITE);
+
+                nameLabel.setPreferredSize(new Dimension(200, 30));
+
+                nameLabel.setFont(new Font("Moonrising", Font.BOLD, 14));
+                nameLabel.setForeground(Color.WHITE);
+
+                score.setPreferredSize(new Dimension(30, 30));
+                score.setOpaque(true);
+                score.setHorizontalAlignment(JLabel.CENTER);
+                score.setBackground(Color.decode("#b3823e"));
+                score.setFont(new Font("Moonrising", Font.BOLD, 14));
+                score.setForeground(Color.WHITE);
+                userPanel.add(number);
+                userPanel.add(nameLabel);
+                userPanel.add(score);
+                add(userPanel);
+            }
+        }
+        setPreferredSize(new Dimension(screenWidth, screenHeight));
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+//        g2.drawImage(background,0,0,null);
+        g2.setColor(Color.decode("#8d6731"));
+        g2.setFont(new Font("Moonrising", Font.BOLD, 50));
+        g2.drawString("Highscore", 95, 85);
+//        g2.dispose();
+    }
+}
